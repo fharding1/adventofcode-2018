6dc0aff6c8e56a48465d1b373c8bd6fe5d1bb96c
Franklin Harding <franklinharding0.0@gmail.com>
2018-11-30 22:33:06 -0800

    Day 1 solution

---
+++ day1/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+const inputFileName = "input.txt"
+
+func main() {
+	frequencies, err := parseFile(inputFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	var sum int
+	history := map[int]struct{}{0: struct{}{}}
+
+	for i := 0; true; i++ {
+		sum += frequencies[i%len(frequencies)]
+
+		if _, ok := history[sum]; ok {
+			break
+		}
+
+		history[sum] = struct{}{}
+
+		if i == len(frequencies)-1 {
+			fmt.Println("first sum:", sum)
+		}
+	}
+
+	fmt.Println("first duplicate sum:", sum)
+}
+
+func parseFile(name string) ([]int, error) {
+	input, err := ioutil.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	var numbers []int
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			return nil, err
+		}
+
+		numbers = append(numbers, n)
+	}
+
+	return numbers, nil
+}
