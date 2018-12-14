d3fa4c450d3c34ccb6fd0d5a29628b4d68d39382
Franklin Harding <franklinharding0.0@gmail.com>
2018-12-14 08:38:50 -0800

    Use byte array instead of int array

--- day14/main.go
+++ day14/main.go
@@ -10,42 +10,33 @@ const input = "681901"
 func main() {
 	inputN, _ := strconv.Atoi(input)
 
-	scores := []int{3, 7}
+	scores := []byte("37")
 	elf1 := 0
 	elf2 := 1
 
 	for i := 0; ; i++ {
-		sum := scores[elf1] + scores[elf2]
+		score1, _ := strconv.Atoi(string(scores[elf1]))
+		score2, _ := strconv.Atoi(string(scores[elf2]))
+		sum := score1 + score2
 
 		for _, ch := range strconv.Itoa(sum) {
-			digit, _ := strconv.Atoi(string(ch))
-			scores = append(scores, digit)
+			scores = append(scores, byte(ch))
 		}
 
 		if len(scores) == inputN+10 {
 			fmt.Print("scores of the ten recipes immediately after the number of recipes in your puzzle input: ")
 			for i := inputN; i < inputN+10; i++ {
-				fmt.Print(scores[i%len(scores)])
+				fmt.Print(string(scores[i%len(scores)]))
 			}
 			fmt.Println()
 		}
 
-		elf1 = (elf1 + 1 + scores[elf1]) % len(scores)
-		elf2 = (elf2 + 1 + scores[elf2]) % len(scores)
+		elf1 = (elf1 + 1 + score1) % len(scores)
+		elf2 = (elf2 + 1 + score2) % len(scores)
 
-		if i+len(input) <= len(scores) && join(scores[i:i+len(input)]) == input {
+		if i+len(input) <= len(scores) && string(scores[i:i+len(input)]) == input {
 			fmt.Println("number of recipes that appear on the scoreboard to the left of the score sequence in your puzzle input:", i)
 			break
 		}
 	}
-}
-
-func join(a []int) string {
-	out := make([]byte, len(a))
-
-	for i, v := range a {
-		out[i] = byte(v + '0')
-	}
-
-	return string(out)
 }
